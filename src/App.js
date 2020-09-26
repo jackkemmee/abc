496b58047d9ecc232cbdf9f68c6c53a622564774
ronnayote kemmee <jackkemmee@gmail.com>
2020-09-26 13:11:14 +0700

    css

--- src/App.js
+++ src/App.js
@@ -1,89 +1,84 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Grid, Nav, Image, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./images/logo.png";
+import shirtImg2 from "./images/j.png";
+import Accordion from "react-bootstrap/Accordion";
+import "./App.css";
 
-import React, { Component } from 'react'
-import { Container,Row, Col, Grid,Nav,Image ,Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './images/logo.png'
-import shirtImg2 from './images/j.png'
-import Accordion from 'react-bootstrap/Accordion'
-import './App.css';
-
-
-
-
-class AcordianMenu  extends Component {
-  render () {
+class AcordianMenu extends Component {
+  render() {
     return (
-      <Accordion defaultActiveKey="0">
-<Card>
-<Accordion.Toggle as={Card.Header} eventKey="0">
-  Click me!
-</Accordion.Toggle>
-<Accordion.Collapse eventKey="0">
-  <Card.Body>Hello! I'm the body</Card.Body>
-</Accordion.Collapse>
-</Card>
-<Card>
-<Accordion.Toggle as={Card.Header} eventKey="1">
-  Click me!
-</Accordion.Toggle>
-<Accordion.Collapse eventKey="1">
-  <Card.Body>Hello! I'm another body</Card.Body>
-</Accordion.Collapse>
-</Card>
-</Accordion>
-    )
-  }         
+      <Accordion defaultActiveKey="0"  className="x-Accordion">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Click me!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            Click me!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    );
+  }
 }
 
 class MenuSecondaryPointing extends Component {
-
   render() {
-  
-  return (
-    <div className="Container">
-        
-          <Row className="row-menu">
-            <Col class="">           
-            <img src={logo}  className="logo"/>       
-            </Col>
-            <Col>
+    return (
+      <div className="Container">
+        <Row className="row-menu">
+          <Col class="">
+            <img src={logo} className="logo" />
+          </Col>
+          <Col>
             <Nav className="pull-left" activeKey="/home">
-                <Nav.Item>
-                  <Nav.Link href="/home">CREATE</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-1">ARTIST</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-2">ALL PRODUCTS</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-2">SHOP</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-2">LOGIN</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="disabled" disabled>
-                    CART
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav> 
-            </Col>
-          </Row>
-          <Row>
-            
-            <Col>
-            <img src={shirtImg2} className="justify-content-center" className="shirt" />
-            </Col>
-            <Col>3 of 3
-            <AcordianMenu></AcordianMenu>
-            </Col>
-          </Row>
-      
-    </div>
-  )
-}
+              <Nav.Item>
+                <Nav.Link href="/home">CREATE</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">ARTIST</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">ALL PRODUCTS</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">SHOP</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">LOGIN</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                  CART
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img
+              src={shirtImg2}
+              className="shirt"
+             
+            />
+          </Col>
+          <Col className="acor-menu">
+             <AcordianMenu ></AcordianMenu>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
-export default MenuSecondaryPointing
+export default MenuSecondaryPointing;
